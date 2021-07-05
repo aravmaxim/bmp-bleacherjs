4db1f4673a499c8757ee6839f22e09da70846191
Maxim Arav <aravmaxim@gmail.com>
2021-07-05 19:34:54 +0300

    Added a test for CVE-2018-7640.

--- test/index.js
+++ test/index.js
@@ -98,4 +98,45 @@ describe('header tests', function(){
   });
 });
 
+describe('known CVE for BMP format', function(){
+  describe('CVE-2018-7640', function(){
+    it('cimg-heap-overflow-load_bmp-48378', function(){
+      var bmpObj = bmp();
+      var bmpData = get_pic('CVE-2018-7640_48378.bmp');
+      var result = bmpObj.check(bmpData);
+      assert.equal(result, false);
+      assert.equal(bmpObj.geterror(), 'Bad file size in header found: 268436086 read size: 512');
+    });
+    it('cimg-heap-overflow-load_bmp-48397', function(){
+      var bmpObj = bmp();
+      var bmpData = get_pic('CVE-2018-7640_48397.bmp');
+      var result = bmpObj.check(bmpData);
+      assert.equal(result, false);
+      assert.equal(bmpObj.geterror(), 'Bad file size in header found: 630 read size: 588');
+    });    
+    it('cimg-heap-overflow-load_bmp-48413', function(){
+      var bmpObj = bmp();
+      var bmpData = get_pic('CVE-2018-7640_48413.bmp');
+      var result = bmpObj.check(bmpData);
+      assert.equal(result, false);
+      assert.equal(bmpObj.geterror(), 'Bad file size in header found: -1900170 read size: 407');
+    });
+    
+    it('cimg-heap-overflow-load_bmp-48427', function(){
+      var bmpObj = bmp();
+      var bmpData = get_pic('CVE-2018-7640_48427.bmp');
+      var result = bmpObj.check(bmpData);
+      assert.equal(result, false);
+      assert.equal(bmpObj.geterror(), 'Bad file size in header found: 626 read size: 56');
+    });
+    it('cimg-heap-overflow-load_bmp-48457', function(){
+      var bmpObj = bmp();
+      var bmpData = get_pic('CVE-2018-7640_48457.bmp');
+      var result = bmpObj.check(bmpData);
+      assert.equal(result, false);
+      assert.equal(bmpObj.geterror(), 'Bad file size in header found: 118 read size: 56');
+    });
+  })
+})
+
 
